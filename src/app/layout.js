f1af4a0b5efc0581411ce940819101ed5472c913
Kariaki58 <kariakistephen809@gmail.com>
2025-05-13 12:39:31 +0100

    analytics integrated

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { AuthProvider } from "./auth-provider";
 import { Toaster } from "sonner";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-white text-slate-900`}>
+        <Analytics/>
         <AuthProvider>
           <Navbar />
           <main className="min-h-[calc(100vh-140px)]">{children}</main>
