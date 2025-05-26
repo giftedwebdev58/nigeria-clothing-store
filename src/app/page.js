fdaf677f6a5a7829e183b368d18b31af9d93e45e
Kariaki58 <kariakistephen809@gmail.com>
2025-05-26 23:02:33 +0100

    make some changes

--- src/app/page.js
+++ src/app/page.js
@@ -73,7 +73,7 @@ export default async function Home() {
               View All
             </Link>
           </div>
-          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             {products.map((product) => (
               <ProductCard key={product._id} product={product} />
             ))}
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -95,7 +95,7 @@ export default async function ProductsPage({ searchParams }) {
                 </div>
                 {/* Products grid */}
                 {paginatedProducts.length > 0 ? (
-                        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
                             {paginatedProducts.map((product) => (
                                 <ProductCard key={product._id} product={product} />
                             ))}
